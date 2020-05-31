86dd1c49f801d1d185b657d89dbb674c9651a0e7
theFl00f <celeste.ellerby@gmail.com>
2020-05-30 21:54:07 -0400

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+console.log('hello')
+
+
+//font family? no as option, some prebuilt from google
+//padding slider x 2 dialog
+//padding slider alert text
+//text color, alert bg color
+//border on/off and slider with color
+//border radius slider
+//drop shadow
