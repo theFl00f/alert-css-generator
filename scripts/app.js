fbcd9642fd76e7dc93f2a17310d3b0653895d7a6
theFl00f <celeste.ellerby@gmail.com>
2020-05-31 17:07:31 -0400

    alert message variable and conditional logic applied

--- scripts/app.js
+++ scripts/app.js
@@ -8,3 +8,56 @@ console.log('hello')
 //border on/off and slider with color
 //border radius slider
 //drop shadow
+
+const $fontFamilySelection = $('#fontFamily')
+const $alertColor = $('#alertBoxColor');
+const $alertBox = $('.alertBox');
+const $alertButton = $('.dismiss');
+const $alertMessage = $('#alertMessage')
+const $alertMessageOut = $('.alertMessage');
+const $forms = $('form')
+
+
+$forms.on('submit', (e) => {
+    e.preventDefault()
+})
+
+$fontFamilySelection.on('change', () => {
+    getFontFamily($alertBox)
+})
+
+getFontFamily = (element) => {
+        element.removeClass('lato montserrat raleway roboto').addClass($fontFamilySelection.val())
+}
+
+$alertColor.on('change', () => {
+    getAlertColor()
+})
+
+getAlertColor = () => {
+    $alertBox.css('backgroundColor', $alertColor.val())
+}
+
+$alertMessage.on('keyup', () => {
+    getMessage();
+})
+
+
+getMessage = () => {
+    $alertMessageOut.text( $alertMessage.val() ) 
+    if ($alertMessage.val() === '') {
+        $alertMessageOut.css('padding', '0')
+    }
+}
+
+getPrev = () => {
+    getFontFamily($alertBox)
+    getAlertColor()
+    getMessage()
+}
+
+$( document ).ready(() => {
+    getPrev()
+})
+
+//going to need a foreach for the inputs to add event listeners for each input in a functional way
